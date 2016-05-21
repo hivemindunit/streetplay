f61d6863352d0d345ff32bbaea31e46842b374f7
Alexander Tsimbalistov <atsimbalistov@gmail.com>
2016-05-21 07:21:46 +0300

    Turned off email confirmation for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable#, :confirmable
 
   has_many :owns, class_name: 'Activity'
   has_and_belongs_to_many :participates, class_name: 'Activity', join_table: :user_participates
