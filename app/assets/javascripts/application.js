6d48cedb1a1526c74d73fc2ec334654c9ad4fa2c
Alexander Tsimbalistov <atsimbalistov@gmail.com>
2016-05-21 17:34:46 +0300

    Added saving of map center, datetimepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,49 @@
 //= require semantic-ui
 //= require underscore
 //= require gmaps/google
+//= require jquery.datetimepicker.min
 //= require_tree .
+// возвращает cookie с именем name, если есть, если нет, то undefined
+
+function getCookie(name) {
+    var matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function setCookie(name, value, options) {
+    options = options || {};
+
+    var expires = options.expires;
+
+    if (typeof expires == "number" && expires) {
+        var d = new Date();
+        d.setTime(d.getTime() + expires * 1000);
+        expires = options.expires = d;
+    }
+    if (expires && expires.toUTCString) {
+        options.expires = expires.toUTCString();
+    }
+
+    value = encodeURIComponent(value);
+
+    var updatedCookie = name + "=" + value;
+
+    for (var propName in options) {
+        updatedCookie += "; " + propName;
+        var propValue = options[propName];
+        if (propValue !== true) {
+            updatedCookie += "=" + propValue;
+        }
+    }
+
+    document.cookie = updatedCookie;
+}
+
+Date.parseDate = function( input, format ){
+    return moment(input,format).toDate();
+};
+Date.prototype.dateFormat = function( format ){
+    return moment(this).format(format);
+};
